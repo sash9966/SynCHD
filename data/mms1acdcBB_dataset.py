c9afece1d8b196bc69e61ec58fb6df3805123094
sash9966 <sascha.stocker@hotmail.ch>
2024-05-31 15:57:38 -0700

    path change to normed 3D images

--- data/mms1acdcBB_dataset.py
+++ data/mms1acdcBB_dataset.py
@@ -57,7 +57,7 @@ class Mms1acdcBBDataset(BaseDataset):
         
         parser.add_argument('--label_dir', type=str, required=False, default = "/scratch/users/fwkong/SharedData/imageCHDCleanedOriginal_aligned_all/aligned/seg_nii_gz_only",
                             help='path to the directory that contains label images')
-        parser.add_argument('--image_dir', type=str, required=False, default ="/scratch/users/fwkong/SharedData/imageCHDCleanedOriginal_aligned_all/aligned/img",
+        parser.add_argument('--image_dir', type=str, required=False, default ="/scratch/users/fwkong/SharedData/imageCHDCleanedOriginal_aligned_all/aligned/normed_img",
                             help='path to the directory that contains photo images')
         
         # parser.add_argument('--label_dir_B', type=str, required=False, default = "/Users/saschastocker/Desktop/Data/StyleTransfer/segmentationTestFullResolution",
