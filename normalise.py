b5df30e41ce62ea7b57d1616f3937623bb403834
sash9966 <71472959+sash9966@users.noreply.github.com>
2024-06-27 14:26:25 -0700

    normalise script added to sherlock git repo

---
+++ normalise.py
@@ -0,0 +1,66 @@
+# %%
+#Load nifti file, transfomr to numpy, apply the normaliastion and save in different path
+
+
+import os
+import SimpleITK as sitk
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# %%
+import numpy as np
+
+def normalize_3d_voxel(input_data, range=(-1, 1), percentiles=(1, 99)):
+    """
+    Normalize a 3D voxel with percentiles.
+
+    :param input_data: 3D numpy array to be normalized.
+    :param range: min and max output values.
+    :param percentiles: lower and upper percentile to clip the data.
+    :return: Normalized 3D numpy array.
+    """
+    
+    # Ensure input_data is a numpy array
+    if not isinstance(input_data, np.ndarray):
+        raise ValueError("Input data must be a numpy array.")
+    
+    # Get the cutoff values for the entire 3D voxel
+    cutoff = np.percentile(input_data, percentiles)
+    
+    # Clip the voxel values based on the cutoff
+    np.clip(input_data, *cutoff, out=input_data)
+    
+    # Normalize the entire 3D voxel
+    in_min = input_data.min()
+    in_max = input_data.max()
+    out_min, out_max = range
+    
+    input_data -= in_min
+    if in_max - in_min == 0:
+        # handle this case accordingly; e.g., set all values to out_min
+        input_data[:] = out_min
+    else:
+        input_data /= (in_max - in_min)
+        input_data *= (out_max - out_min)
+        input_data += out_min
+
+    return input_data
+
+
+#folder path:
+path = '/scratch/users/sastocke/data/unlabeled/cropped_128'
+output = '/scratch/users/sastocke/data/refimages'
+
+#list of files in folder
+files = os.listdir(path)
+for file in files:
+    img = sitk.ReadImage(os.path.join(path,file))
+    img = sitk.GetArrayFromImage(img)
+    img = img.astype(np.float32)
+    img_norm = normalize_3d_voxel(img)
+    sitk.WriteImage(sitk.GetImageFromArray(img_norm), os.path.join(output,file))
+# %%
+
+
+
