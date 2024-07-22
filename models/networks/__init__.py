659aab3df345e6800ddfe6bd1504f3e881b39f06
sash9966 <71472959+sash9966@users.noreply.github.com>
2024-07-22 14:51:51 -0700

    checking path for utils import, shouuld sovle issue on sherlcok, ultron and local

--- models/networks/__init__.py
+++ models/networks/__init__.py
@@ -7,13 +7,15 @@ Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses
 import importlib.util
 
 # BUG: Unsure why import fails, this fixed it, from chatgpt
+import os 
+curr_path= os.path.getcwd()
 try:
-    path_to_util = "/home/sastocke/2Dslicesfor3D/util/util.py"
+    path_to_util = f"{curr_path}/util/util.py"
     spec = importlib.util.spec_from_file_location("util", path_to_util)
     util = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(util)
 except:
-    path_to_util = "/home/users/sastocke/2Dslicesfor3D/util/util.py"
+    path_to_util = f"{curr_path}/util/util.py"
     spec = importlib.util.spec_from_file_location("util", path_to_util)
     util = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(util)
